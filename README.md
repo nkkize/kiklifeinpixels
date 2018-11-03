442edad38b5d756d641889f3d313bd3ae1eac973
nkkize <nkumar2@onemarketnetwork.com>
2018-11-04 00:05:21 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Life in Pixels: a kik bot
+
+## Dependencies
+Make sure you're running at least npm 2.2.1 and Node 8.0.0.
+
+## Environment Variables
+
+```bash
+#service listen port
+serverPort=${serverPort}
+#The username you gave BotsWorth on Kik
+username=${username}
+# The API Key you can find on your profile on dev.kik.com
+apiKey=${apiKey}
+# The webhook, the the web tunnel or host you have running
+baseUrl=${baseUrl}/incoming
+
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+let util = require('util'); 
+let http = require('http');
+let Bot  = require('@kikinteractive/kik');
+
+let bot = new Bot({
+    username: process.env.username, 
+    apiKey: process.env.apiKey,
+    baseUrl: process.env.baseUrl, 
+});
+
+// Send the configuration to kik
+bot.updateBotConfiguration();
+
+// The onTextMessage(message) handler
+bot.onTextMessage((message) => {
+
+    message.reply(message.body);
+    console.log(message.body);
+});
+
+// start chatting message handler. This message is only sent once. 
+bot.onStartChattingMessage((message) => {
+    bot.getUserProfile(message.from)
+        .then((user) => {
+            message.reply(`Hey ${user.firstName}! How can I help you?`);
+        });
+});
+
+// Set up your server
+let server = http
+    .createServer(bot.incoming())
+    .listen(9093, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on port 9093`)
+});
